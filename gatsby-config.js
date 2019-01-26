6aaa76cee883224f366f63e196ba415fac3b89e3
Matt Horrall <mhorrall@gmail.com>
2019-01-26 17:13:12 -0600

    add 'gatsby-plugin-remove-serviceworker'

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     }
   },
   plugins: [
+    'gatsby-plugin-remove-serviceworker',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`, {
       resolve: `gatsby-source-filesystem`,
